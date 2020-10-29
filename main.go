888143f05de1caca8749cebe2833c9e608dfc01e
Vitaly Uvarov <vitalyu@gmail.com>
2020-10-29 20:34:25 +0000

    merge fixes

--- main.go
+++ main.go
@@ -9,47 +9,19 @@ import (
 	"os"
 	"runtime"
 	"strconv"
-<<<<<<< HEAD
-=======
 	"time"
->>>>>>> v.1.12.1-dodo
 
 	"github.com/prometheus/client_golang/prometheus"
 	log "github.com/sirupsen/logrus"
 )
 
 var (
-<<<<<<< HEAD
-	redisAddr        = flag.String("redis.addr", getEnv("REDIS_ADDR", ""), "Address of one or more redis nodes, separated by separator")
-	redisFile        = flag.String("redis.file", getEnv("REDIS_FILE", ""), "Path to file containing one or more redis nodes, separated by newline. NOTE: mutually exclusive with redis.addr")
-	redisPassword    = flag.String("redis.password", getEnv("REDIS_PASSWORD", ""), "Password for one or more redis nodes, separated by separator")
-	redisAlias       = flag.String("redis.alias", getEnv("REDIS_ALIAS", ""), "Redis instance alias for one or more redis nodes, separated by separator")
-	namespace        = flag.String("namespace", getEnv("REDIS_EXPORTER_NAMESPACE", "redis"), "Namespace for metrics")
-	checkKeys        = flag.String("check-keys", getEnv("REDIS_EXPORTER_CHECK_KEYS", ""), "Comma separated list of key-patterns to export value and length/size, searched for with SCAN")
-	checkSingleKeys  = flag.String("check-single-keys", getEnv("REDIS_EXPORTER_CHECK_SINGLE_KEYS", ""), "Comma separated list of single keys to export value and length/size")
-	scriptPath       = flag.String("script", getEnv("REDIS_EXPORTER_SCRIPT", ""), "Path to Lua Redis script for collecting extra metrics")
-	separator        = flag.String("separator", getEnv("REDIS_EXPORTER_SEPARATOR", ","), "separator used to split redis.addr, redis.password and redis.alias into several elements.")
-	listenAddress    = flag.String("web.listen-address", getEnv("REDIS_EXPORTER_WEB_LISTEN_ADDRESS", ":9121"), "Address to listen on for web interface and telemetry.")
-	metricPath       = flag.String("web.telemetry-path", getEnv("REDIS_EXPORTER_WEB_TELEMETRY_PATH", "/metrics"), "Path under which to expose metrics.")
-	isDebug          = flag.Bool("debug", getEnvBool("REDIS_EXPORTER_DEBUG"), "Output verbose debug information")
-	logFormat        = flag.String("log-format", getEnv("REDIS_EXPORTER_LOG_FORMAT", "txt"), "Log format, valid options are txt and json")
-	showVersion      = flag.Bool("version", false, "Show version information and exit")
-	useCfBindings    = flag.Bool("use-cf-bindings", getEnvBool("REDIS_EXPORTER_USE-CF-BINDINGS"), "Use Cloud Foundry service bindings")
-	redisMetricsOnly = flag.Bool("redis-only-metrics", getEnvBool("REDIS_EXPORTER_REDIS_ONLY_METRICS"), "Whether to export go runtime metrics also")
-	useAzureCaches   = flag.Bool("use-azure-caches", false, "Use azure api to obtain targets")
-
-	// VERSION, BUILD_DATE, GIT_COMMIT are filled in by the build script
-	VERSION     = "<<< filled in by build >>>"
-	BUILD_DATE  = "<<< filled in by build >>>"
-	COMMIT_SHA1 = "<<< filled in by build >>>"
-=======
 	/*
 		BuildVersion, BuildDate, BuildCommitSha are filled in by the build script
 	*/
 	BuildVersion   = "<<< filled in by build >>>"
 	BuildDate      = "<<< filled in by build >>>"
 	BuildCommitSha = "<<< filled in by build >>>"
->>>>>>> v.1.12.1-dodo
 )
 
 func getEnv(key string, defaultVal string) string {
@@ -59,13 +31,6 @@ func getEnv(key string, defaultVal string) string {
 	return defaultVal
 }
 
-<<<<<<< HEAD
-func getEnvBool(key string) (envValBool bool) {
-	if envVal, ok := os.LookupEnv(key); ok {
-		envValBool, _ = strconv.ParseBool(envVal)
-	}
-	return
-=======
 func getEnvBool(key string, defaultVal bool) bool {
 	if envVal, ok := os.LookupEnv(key); ok {
 		envBool, err := strconv.ParseBool(envVal)
@@ -74,7 +39,6 @@ func getEnvBool(key string, defaultVal bool) bool {
 		}
 	}
 	return defaultVal
->>>>>>> v.1.12.1-dodo
 }
 
 func main() {
@@ -148,20 +112,7 @@ func main() {
 		if err != nil {
 			log.Fatalf("Couldn't load TLS client key pair, err: %s", err)
 		}
-<<<<<<< HEAD
-	case *useCfBindings:
-		addrs, passwords, aliases = exporter.GetCloudFoundryRedisBindings()
-	case *useAzureCaches:
-		var err error
-		addrs, passwords, aliases, err = exporter.GetAzureRedisServices()
-		if err != nil {
-			log.Fatal(err)
-		}
-	default:
-		addrs, passwords, aliases = exporter.LoadRedisArgs(*redisAddr, *redisPassword, *redisAlias, *separator)
-=======
 		tlsClientCertificates = append(tlsClientCertificates, cert)
->>>>>>> v.1.12.1-dodo
 	}
 
 	var tlsCaCertificates *x509.CertPool
